0754e16233590982af74c180881128091704c152
joseph <josephdesoissan"gmail.com>
2017-11-24 00:28:40 -0500

    Switch css to scss, add Flexboxgrid

--- volontaria/src/app/components/footer/footer.component.ts
+++ volontaria/src/app/components/footer/footer.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent implements OnInit {
--- volontaria/src/app/components/header/header.component.ts
+++ volontaria/src/app/components/header/header.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
