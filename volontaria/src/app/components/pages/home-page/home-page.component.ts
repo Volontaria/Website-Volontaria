44deaf3446057b6a00a14c3f48a8eaa08f058756
joseph <josephdesoissan"gmail.com>
2017-11-25 16:44:34 -0500

    Home page content

--- volontaria/src/app/components/pages/home-page/home-page.component.ts
+++ volontaria/src/app/components/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class HomePageComponent {
