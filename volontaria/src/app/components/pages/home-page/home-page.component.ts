2af0b628d4005f40d9437773306c122df68b22e1
RignonNoel <rignon.noel@gmail.com>
2018-01-03 21:11:14 -0500

    set username on homepage

--- volontaria/src/app/components/pages/home-page/home-page.component.ts
+++ volontaria/src/app/components/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {Cycle} from "../../../models/cycle";
 import {CycleService} from "../../../services/cycle.service";
+import {User} from "../../../models/user";
+import {UserService} from "../../../services/user.service";
 
 @Component({
   selector: 'home-page',
@@ -11,8 +13,16 @@ import {CycleService} from "../../../services/cycle.service";
 export class HomePageComponent {
 
   cycles: Cycle[];
+  user: User;
 
-  constructor(private cycleService:CycleService) {
+  constructor(private cycleService:CycleService,
+              private userService:UserService)
+  {
+    this.userService.getProfile().subscribe(
+      data => {
+        this.user = data;
+      }
+    );
     this.cycleService.getCycles().subscribe(
       data => {
         this.cycles = data.results.map(c => new Cycle(c) );
