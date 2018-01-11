3dfcda453f6e2b0c69fdbb1e36b8a941eb6d498f
RignonNoel <rignon.noel@gmail.com>
2018-01-11 17:22:52 -0500

    add contact page

--- volontaria/src/app/components/pages/info-page/info-page.component.ts
+++ volontaria/src/app/components/pages/info-page/info-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'info-page.component.html',
-  selector: 'info-page'
+  selector: 'info-page',
+  styleUrls: ['info-page.component.scss'],
 })
 export class InfoPageComponent {
 
