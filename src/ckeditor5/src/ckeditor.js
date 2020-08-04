a0470bf6a0ef2e0058b38a83ae11324d58348119
Noel Rignon <noel.rignon@fjnr.ca>
2020-08-04 12:36:51 -0400

    update ckeditor with new features

--- src/ckeditor5/src/ckeditor.js
+++ src/ckeditor5/src/ckeditor.js
@@ -3,96 +3,129 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
 import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
-import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 export default class Editor extends InlineEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
+  Alignment,
   Autoformat,
-  Bold,
-  Italic,
+  Autosave,
   BlockQuote,
+  Bold,
   CKFinder,
   CKFinderUploadAdapter,
+  Essentials,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
   Heading,
+  Highlight,
+  HorizontalLine,
   Image,
   ImageCaption,
+  ImageResize,
   ImageStyle,
   ImageToolbar,
   ImageUpload,
   Indent,
+  IndentBlock,
+  Italic,
   Link,
   List,
   MediaEmbed,
+  MediaEmbedToolbar,
+  Paragraph,
   PasteFromOffice,
   Table,
+  TableCellProperties,
+  TableProperties,
   TableToolbar,
-  Autosave,
-  FontFamily,
-  FontSize,
-  FontColor,
-  FontBackgroundColor,
-  ImageResize,
-  Essentials,
-  Paragraph,
+  TextTransformation,
+  TodoList,
+  Underline,
 ];
 
 Editor.defaultConfig = {
   toolbar: {
     items: [
+      'heading',
       'fontFamily',
       'fontSize',
-      'fontColor',
       '|',
+      'fontColor',
+      'fontBackgroundColor',
+      'highlight',
+      '|',
+      'alignment',
       'bold',
       'italic',
+      'underline',
       'link',
-      'blockQuote',
+      'bulletedList',
+      'numberedList',
+      'todoList',
       '|',
+      'indent',
+      'outdent',
+      '|',
+      'horizontalLine',
+      'blockQuote',
+      'insertTable',
       'undo',
       'redo',
     ],
   },
-  alignment: {
-    options: ['left', 'right', 'center', 'justify'],
+  language: 'fr',
+  image: {
+    toolbar: ['imageTextAlternative', 'imageStyle:full', 'imageStyle:side'],
   },
-  fontSize: {
-    options: ['default', 15, 20],
-  },
-  fontColor: {
-    colors: [
-      {
-        color: '#142823',
-        label: '$nt-text',
-      },
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells',
+      'tableCellProperties',
+      'tableProperties',
     ],
   },
-  language: 'fr',
+  licenseKey: '',
 };
